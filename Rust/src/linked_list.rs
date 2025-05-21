7feaaa5a2d8792534ec0b1f4b843a9191daa1365
Xiaode Zheng <your.email@example.com>
2025-05-22 01:54:39 +0800

    rust linked list

--- Rust/src/linked_list.rs
+++ Rust/src/linked_list.rs
@@ -1,55 +1,193 @@
 use std::cell::RefCell;
+use std::io::Cursor;
 use std::rc::Rc;
 
+type Link<T> = Option<Rc<RefCell<ListNode<T>>>>;
+
 #[derive(Debug)]
-pub struct ListNode {
-    pub val: i32,
-    pub next: Option<Rc<RefCell<ListNode>>>,
+pub struct ListNode<T> {
+    pub val: T,
+    pub next: Link<T>,
 }
 
-#[allow(non_snake_case)]
-pub fn insert(n0: &Rc<RefCell<ListNode>>, P: Rc<RefCell<ListNode>>) {
-    let n1 = n0.borrow_mut().next.take();
-    P.borrow_mut().next = n1;
-    n0.borrow_mut().next = Some(P);
-}
-
-#[allow(non_snake_case)]
-pub fn remove(n0: &Rc<RefCell<ListNode>>) {
-    let P = n0.borrow_mut().next.take();
-    if let Some(node) = P {
-        let n1: Option<Rc<RefCell<ListNode>>> = node.borrow_mut().next.take();
-        n0.borrow_mut().next = n1;
+impl<T> ListNode<T> {
+    fn new(val: T) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(ListNode { val, next: None }))
     }
 }
 
-pub fn access(head: Rc<RefCell<ListNode>>, index: i32) -> Option<Rc<RefCell<ListNode>>> {
-    fn dfs(head: Option<&Rc<RefCell<ListNode>>>, index: i32) -> Option<Rc<RefCell<ListNode>>> {
-        if index <= 0 {
-            return head.cloned();
-        }
+#[derive(Debug)]
+pub struct LinkedList<T> {
+    head: Link<T>,
+    tail: Link<T>,
+}
 
-        if let Some(node) = head {
-            dfs(node.borrow().next.as_ref(), index - 1)
-        } else {
-            None
+impl<T: PartialEq> LinkedList<T> {
+    pub fn new() -> Self {
+        Self {
+            head: None,
+            tail: None,
         }
     }
 
-    dfs(Some(head).as_ref(), index)
-}
+    pub fn prepend(&mut self, value: T) {
+        let new_node = ListNode::new(value);
+        new_node.borrow_mut().next = self.head.clone();
+        self.head = Some(new_node.clone());
 
-pub fn find(head: Rc<RefCell<ListNode>>, target: i32) -> i32 {
-    fn find_inner(head: Option<&Rc<RefCell<ListNode>>>, target: i32, index: i32) -> i32 {
-        if let Some(node) = head {
-            if node.borrow().val == target {
-                return index;
+        if self.tail.is_none() {
+            self.tail = Some(new_node);
+        }
+    }
+
+    pub fn append(&mut self, value: T) {
+        let new_node = ListNode::new(value);
+
+        match self.tail.take() {
+            Some(old_tail) => {
+                old_tail.borrow_mut().next = Some(new_node.clone());
             }
-            find_inner(node.borrow().next.as_ref(), target, index + 1)
-        } else {
-            -1
+            None => {
+                self.head = Some(new_node.clone());
+            }
         }
+        self.tail = Some(new_node);
     }
 
-    find_inner(Some(head).as_ref(), target, 0)
+    pub fn insert(&mut self, value: T, raw_index: usize) {
+        if raw_index == 0 {
+            self.prepend(value);
+            return;
+        }
+
+        let mut index = 1;
+        let mut current = self.head.clone();
+
+        while let Some(node) = current.clone() {
+            if index == raw_index {
+                let new_node = ListNode::new(value);
+                new_node.borrow_mut().next = node.borrow_mut().next.take();
+                node.borrow_mut().next = Some(new_node.clone());
+
+                if new_node.borrow().next.is_none() {
+                    self.tail = Some(new_node);
+                }
+                return;
+            }
+            current = node.borrow().next.clone();
+            index += 1;
+        }
+        self.append(value);
+    }
+
+    pub fn delete(&mut self, value: T) {
+        while let Some(head_node) = self.head.clone() {
+            if head_node.borrow().val == value {
+                self.head = head_node.borrow_mut().next.take();
+            } else {
+                break;
+            }
+        }
+
+        let mut current = self.head.clone();
+
+        while let Some(current_node) = current.clone() {
+            let mut next_opt = current_node.borrow().next.clone();
+            while let Some(next_node) = next_opt.clone() {
+                if next_node.borrow().val == value {
+                    current_node.borrow_mut().next = next_node.borrow_mut().next.take();
+                } else {
+                    break;
+                }
+                next_opt = current_node.borrow().next.clone();
+            }
+            current = current_node.borrow().next.clone();
+        }
+        let mut tail = self.head.clone();
+        while let Some(tail_node) = tail.clone() {
+            if tail_node.borrow().next.is_none() {
+                self.tail = Some(tail_node);
+                return;
+            }
+            tail = tail_node.borrow().next.clone();
+        }
+        self.tail = None;
+    }
+
+    pub fn find(&self, target: T) -> Link<T>
+    where
+        T: Clone,
+    {
+        let mut current = self.head.clone();
+        while let Some(current_node) = current.clone() {
+            if current_node.borrow().val == target {
+                return Some(current_node);
+            }
+            current = current_node.borrow().next.clone();
+        }
+        None
+    }
+
+    pub fn find_index(&self, target: T) -> Option<usize>
+    where
+        T: Clone,
+    {
+        let mut current = self.head.clone();
+        let mut index: usize = 0;
+        while let Some(current_node) = current.clone() {
+            if current_node.borrow().val == target {
+                return Some(index);
+            }
+            current = current_node.borrow().next.clone();
+            index += 1;
+        }
+        None
+    }
+
+    pub fn reverse(&mut self) {
+        let mut prev = None;
+        let mut current = self.head.clone();
+
+        self.tail = self.head.clone();
+
+        while let Some(current_node) = current.clone() {
+            let next = current_node.borrow_mut().next.take();
+            current_node.borrow_mut().next = prev.clone();
+            prev = Some(current_node.clone());
+            current = next;
+        }
+        self.head = prev;
+    }
+
+    pub fn to_vec(&self) -> Vec<T>
+    where
+        T: Clone,
+    {
+        let mut vec = Vec::new();
+        let mut current = self.head.clone();
+
+        while let Some(current_node) = current {
+            vec.push(current_node.borrow().val.clone());
+            current = current_node.borrow().next.clone();
+        }
+        vec
+    }
+
+    pub fn to_string<F>(&self, callback: Option<F>) -> String
+    where
+        T: std::fmt::Debug + Clone,
+        F: Fn(&T) -> String,
+    {
+        self.to_vec()
+            .iter()
+            .map(|v| {
+                if let Some(ref cb) = callback {
+                    cb(v)
+                } else {
+                    format!("{:?}", v)
+                }
+            })
+            .collect::<Vec<_>>()
+            .join(", ")
+    }
 }
--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,23 +1,16 @@
-use std::cell::RefCell;
-use std::rc::Rc;
-
-use linked_list::ListNode;
-use linked_list::find;
-
 mod array;
 mod linked_list;
 
 fn main() {
-    let n0: Rc<RefCell<ListNode>> = Rc::new(RefCell::new(ListNode { val: 1, next: None }));
-    let n1: Rc<RefCell<ListNode>> = Rc::new(RefCell::new(ListNode { val: 2, next: None }));
-    let n2: Rc<RefCell<ListNode>> = Rc::new(RefCell::new(ListNode { val: 3, next: None }));
-    let n3: Rc<RefCell<ListNode>> = Rc::new(RefCell::new(ListNode { val: 4, next: None }));
-    let n4: Rc<RefCell<ListNode>> = Rc::new(RefCell::new(ListNode { val: 5, next: None }));
-
-    n0.borrow_mut().next = Some(n1.clone());
-    n1.borrow_mut().next = Some(n2.clone());
-    n2.borrow_mut().next = Some(n3.clone());
-    n3.borrow_mut().next = Some(n4.clone());
-
-    println!("{}", find(n0, 3));
+    let mut linked_list = linked_list::LinkedList::new();
+    linked_list.prepend(1);
+    linked_list.prepend(2);
+    linked_list.prepend(3);
+    // println!("{:?}", linked_list);
+    linked_list.append(4);
+    // println!("{:?}", linked_list);
+    linked_list.delete(4);
+    // println!("{:?}", linked_list);
+    linked_list.reverse();
+    println!("{:?}", linked_list.to_string(None::<fn(&i32) -> String>));
 }
